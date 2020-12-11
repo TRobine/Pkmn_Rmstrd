aef381de3890a6cd3aed97c53231faee968722be
Felix Soubelet <felix.soubelet@protonmail.com>
2020-12-11 10:55:32 +0100

    that would be version update

--- pokejdr/_version.py
+++ pokejdr/_version.py
@@ -1,7 +1,7 @@
 __title__ = "pokejdr"
 __description__ = "Un package utilitaire pour notre JDR pokemon."
 __url__ = "https://github.com/TRobine/Pkmn_Rmstrd"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "T. Robine & F. Soubelet"
 __author_email__ = ""
 __license__ = "MIT"
